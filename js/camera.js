dc10b045aa8d6da4f51294ab43956d386e04afa0
notiontaxi <florian.wokurka@gmail.com>
2013-09-06 22:39:38 +0200

    debugging

--- js/camera.js
+++ js/camera.js
@@ -4,7 +4,7 @@ $(function() {
   var $canvas = $('#cam-canvas')
 
   var pictureSource
-  var destinationType
+  var destinationType
   var image
 
   $('#cam-action').click(function(e){
@@ -16,18 +16,17 @@ $(function() {
       drawPicture, 
       onError, 
       { 
-        quality: 50 ,
-        destinationType: destinationType.DATA_URL 
+        quality: 50 
       }
-    )}
-  )
+      );
+  })
 
 
   // Wait for cordova
   function onDeviceReady() {
     console.log('starting camera example');
     pictureSource = navigator.camera.PictureSourceType
-    destinationType = navigator.camera.DestinationType
+    destinationType = navigator.camera.DestinationType
   }
 
   function drawPicture(data){
