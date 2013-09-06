557bdafff9a6fc3fac599d6065005e02ca6a74da
notiontaxi <florian.wokurka@gmail.com>
2013-09-06 19:19:16 +0200

    added camera example

---
+++ js/camera.js
@@ -0,0 +1,36 @@
+
+$(function() {
+
+  var $canvas = $('#cam-canvas')
+
+  var pictureSource
+  var destinationType
+  var image
+
+  $('#cam-action').click(function(e){
+    document.addEventListener("deviceready", onDeviceReady, false);
+  });
+
+  $('#take-photo').click(function(e){
+    navigator.camera.getPicture(drawPicture, onError, { quality: 50 });
+  })
+
+
+  // Wait for cordova
+  function onDeviceReady() {
+    console.log('starting camera example');
+    pictureSource = navigator.camera.PictureSourceType
+    destinationType = navigator.camera.DestinationType
+  }
+
+  function drawPicture(data){
+    image = $('picture-frame')[0]
+    smallImage.style.display = 'block'
+    smallImage.src = "data:image/jpeg;base64," + data
+  }
+
+  function onError(e) {
+      console.log(e);
+  }
+
+});
